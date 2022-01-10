01ca40b3be002e9efceab54d83477e954e7f2d43
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-01-10 17:04:51 +0300

    fixed some errors

--- 4,1.js
+++ 4,1.js
@@ -61,11 +61,11 @@ function sum(obj){
 console.log(sum(zpha))
 
 
-
-/* Создайте функцию multiplyNumeric(obj), которая умножает все 
+/* 
+Создайте функцию multiplyNumeric(obj), которая умножает все 
 числовые свойства объекта obj на 2.
 Например:
-// до вызова функции
+// до вызова функции */
 let menu = {
   width: 200,
   height: 300,
@@ -73,12 +73,20 @@ let menu = {
 };
 multiplyNumeric(menu);
 // после вызова функции
-menu = {
+/* menu = {
   width: 400,
   height: 600,
   title: "My menu"
-};
+}; */
 
-Обратите внимание, что multiplyNumeric не нужно ничего возвращать.
+/* Обратите внимание, что multiplyNumeric не нужно ничего возвращать.
  Следует напрямую изменять объект.
 P.S. Используйте typeof для проверки, что значение свойства числовое. */
+
+function multiplyNumeric(obj){
+    for (let key in obj){
+       if ((typeof obj[key])=="number"){
+        obj[key]=obj[key]*2;
+       }
+    }
+}
