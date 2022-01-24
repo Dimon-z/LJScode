2d105a8824fe83c8ff921be698c2558adc2bb091
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-01-24 19:06:07 +0300

    поправил кодстайл.?

--- metodiMassivov.js
+++ metodiMassivov.js
@@ -3,16 +3,16 @@
 /* zadacha1 */
 
 function moveToStart(array,n){
-let length = array.length;
-let newarray=[];
-if(n<array.length){
-newarray=array.slice(-n,length)+","+array.slice(0,length-n);
-return newarray;
-}
-else{
-newarray=array.slice(0,length);
-return newarray;
-}
+    let length = array.length;
+    let newarray=[];
+    if(n<array.length){
+        newarray=array.slice(-n,length)+","+array.slice(0,length-n);
+        return newarray;
+    } else {
+        newarray=array.slice(0,length);
+        return newarray;
+    }
+
 }
 
 let arr=[1,2,3,4,5,6,7,8,];
@@ -23,7 +23,7 @@ let arr=[1,2,3,4,5,6,7,8,];
 /* zadacha 2 */
 
 function hasArrays(array){
-return array.some(Array.isArray)
+    return array.some(Array.isArray)
 }
 
 /* zadacha 2 end */
@@ -39,6 +39,7 @@ function getNumbersByParity(array,str){
         case "odd":
         return array.filter(value=>value%2===1);
     }
+
 }   
 
 /* zadacha 3 end */
@@ -47,13 +48,17 @@ function getNumbersByParity(array,str){
 function sequenceSum(start,end){
     if (start>end) {
         return NaN
-    } else {
-if (start===end) {
-    return end;
-} else {
-    return start+sequenceSum(start+1,end);
-}
-}
+    }
+
+    else {
+        if (start===end) {
+            return end;
+        } else {
+            return start+sequenceSum(start+1,end);
+        }
+
+    }
+
 }
 /* zadacha 4 end */
 
@@ -68,10 +73,13 @@ let obj = {
 
 function getStringCount(obj){
         if (typeof(obj)=="string") {
-        return 1;}
-        if (typeof(obj)!=="object"){
-        return 0;
+            return 1;
         }
+
+        if (typeof(obj)!=="object"){
+            return 0;
+        }
+        
     return Object.values(obj).reduce((accum,current)=>accum+getStringCount(current),0)
 }
 
